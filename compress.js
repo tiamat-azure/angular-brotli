fed62230d5cade577aea23f038b2093e8ce7dd49
Tiamat Azure <tiamat.azure@gmail.com>
2019-03-23 10:34:42 +1100

    feat(init): Initialization of the Angular project with brotli compression.

---
+++ compress.js
@@ -0,0 +1,19 @@
+const brotli = require('brotli')
+const fs = require('fs')
+
+const brotliSettings = {
+  extension: 'br',
+  skipLarger: true,
+  mode: 1, // 0 = generic, 1 = text, 2 = font (WOFF2)
+  quality: 11, // 0 - 11,
+  lgwin: 12 // default
+}
+
+const path = 'dist/angular-brotli';
+
+fs.readdirSync(path).forEach(file => {
+  if (file.endsWith('.js') || file.endsWith('.css') || file.endsWith('.html')) {
+    const result = brotli.compress(fs.readFileSync(path + '/' + file), brotliSettings)
+    fs.writeFileSync(path + '/' + file + '.br', result)
+  }
+})
