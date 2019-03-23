a3755406e45f1b699db5cdc35d0d60eb8611a512
Tiamat Azure <tiamat.azure@gmail.com>
2019-03-23 20:21:36 +1100

    add e2e test headless

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
-    }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
