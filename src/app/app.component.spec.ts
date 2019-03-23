2dd7becac8a88912646f46f2e4888d0dd8636879
Tiamat Azure <tiamat.azure@gmail.com>
2019-03-23 20:08:18 +1100

    refactor test setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,20 +14,24 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  function setup() {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+    return { fixture, app };
+  }
+
+  it('should create the app', () => {
+    const { app } = setup();
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'angular-brotli'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const { app } = setup();
     expect(app.title).toEqual('angular-brotli');
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const { fixture } = setup();
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-brotli!');
